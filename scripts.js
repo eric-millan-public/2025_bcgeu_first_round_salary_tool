1bb151aecd0d4fbe0f5b10e375883ea2d6bfd947
Eric Millan <ericmillan2@gmail.com>
2025-06-20 19:44:35 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -11,6 +11,25 @@ const newWageDisplay = document.getElementById('newWage');
 const dollarIncreaseDisplay = document.getElementById('dollarIncrease');
 const percentIncreaseDisplay = document.getElementById('percentIncrease');
 
+const wageChartData = {
+  years: [
+    2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009,
+    2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019,
+    2020, 2021, 2022, 2023, 2024
+  ],
+  realWages: [
+    20.0, 20.5, 21.01, 21.68, 21.68, 21.68, 22.22, 22.89, 23.46, 23.46,
+    23.46, 23.46, 23.93, 24.41, 24.41, 24.65, 24.77, 25.14, 25.52, 26.29,
+    26.82, 27.36, 28.25, 30.16, 31.06
+  ],
+  inflationWages: [
+    20.0, 20.34, 20.83, 21.29, 21.78, 22.22, 22.6, 23.01, 23.49, 23.49,
+    23.8, 24.37, 24.64, 24.62, 24.87, 25.14, 25.59, 26.13, 26.84, 27.46,
+    27.68, 28.46, 30.42, 31.61, 32.43
+  ]
+};
+
+
 // Populate dropdown
 wageTable.forEach(pos => {
   const option = document.createElement('option');
